d05862da32772ad82857172f55df7caab35dc76e
Jeremy <wellsj5@upmc.edu>
2015-12-08 08:53:59 -0500

    updated v1 resume

--- v1/app.js
+++ v1/app.js
@@ -19,8 +19,8 @@ angular
     return [
       'C#','HTML','CSS','JQuery','JavaScript','AJAX','Windows', 'MQ', 'AJAX',
       'OSX','Ubuntu','Fedora','AngularJs','Jasmine','Bootstrap', 'ESB', 'WCF',
-      'RxJS',	'Emacs', 'vsCode','ASP.NET', 'MVC', 'WebApi', 'WCF', 'WMB',
-      'MSSQL Server', 'MongoDB','TDD', 'BDD',	'jRuby', 'Docker', 'SOAP',
+      'RxJS', 'Emacs', 'vsCode','ASP.NET', 'MVC', 'WebApi', 'WCF', 'WMB',
+      'MSSQL Server', 'MongoDB','TDD', 'BDD', 'jRuby', 'Docker', 'SOAP',
       'Multi-tenant applications','ORM Frameworks', 'Go', 'Ruby on Rails', 
       'Sass','Less','MySQL', 'NodeJS', 'npm', 'Grunt', 'Gulp', 'Yeoman', 
       'Active Record', 'Cucumber', 'Rspec', 'RabbitMQ', 'Linq-To-Sql','Entity Framework'
@@ -48,12 +48,12 @@ angular
       '[PghDevUX.com]': [ 
         { timespan:'10/2015-present', name: '(Co)Organizer' }
       ],
-      '[pghdotnet]': [ 
+      '[pghdotnet meetup]': [ 
         { timespan:'12/2012-present', name: 'Member' },
         { timespan:'07/2014', name: 'Speaker, Jasmine' },
         { timespan:'03/2014', name: 'Speaker, OOP in JS' }
       ],
-      '[node.pgh]': [ 
+      '[node.pgh meetup]': [ 
         { timespan:'04/2014', name: 'Member' }
       ]
     };
@@ -69,10 +69,10 @@ angular
           { point: 'Worked with design teams to create functional SPA prototypes for various design concepts' },
           { point: 'Worked to create a custom Vidyo implementation in AngularJs' }, 
           { point: 'Designed and implemented features and enhancements, ensuring code coverage and maintainability, adhering to SDLC practices' },
-          { point: 'Fixed bugs/defects.' },           
-          { point: 'Helped facilitate multi-tenancy in a large sharded MongoDB cluster' },
-          { point: 'Standardized front end concerns.' },
-          { point: 'Used Ruby on Rails, Active Record, Cucumber, Rspec, MySql, JavaScript, MongoDB, RabbitMQ, Docker' }          
+          { point: 'Helped facilitate multi-tenancy in a large distributed application' },
+          { point: 'Standardized front end concerns' },
+          { point: 'Fixed bugs and defects' },
+          { point: 'Used Ruby on Rails, Active Record, Cucumber, Rspec, MySql, JavaScript, MongoDB, RabbitMQ, Docker, MongoDB, jRuby' }          
         ]
       },
       {
@@ -82,12 +82,11 @@ angular
         bullets: [
           { point: 'Provided expertise in areas of web api and SPA development and design principles' },
           { point: 'Instructed team on AngularJs' },
-          { point: 'Led front end efforts on a federated mobile-first membership application intended to integrate disparate applications data and act as the system of record'}, 
+          { point: 'Led front end efforts on a federated mobile-first application, integrating disparate applications data'}, 
           { point: 'Worked with product owners, gathered software requirements, created documentation' },
-          { point: 'Maintained and enhanced production applications' }, 
-          { point: 'Designed and implemented features and enhancements' },
-          { point: 'Fixed bugs' },
-          { point: 'Used Linq-To-Sql and Entity Framework as data access framework/ORM' }
+          { point: 'Designed, implemented and maintained features and enhancements' },
+          { point: 'Fixed bugs and defects' },
+          { point: 'Used C#, Ajax, Linq-To-Sql, Entity Framework' }
         ]
       },
       {
@@ -95,15 +94,15 @@ angular
         company: 'All Covered', 
         title: 'Senior Technical Engineer',
         bullets: [
-          { point: 'Designed and Implemented Continuous Integration plan and strategy for new and existing code' }, 
-          { point: 'Designed and Implemented deployable artifact creation and versioning'},
-          { point: 'Implemented SDLC and Agile processes in distributed team environments' },
+          { point: 'Designed and implemented continuous integration plan and strategy for new and existing code' }, 
+          { point: 'Designed and implemented deployable artifact creation and versioning'},
+          { point: 'Implemented SDLC and agile processes in distributed team environments' },
           { point: 'Facilitated planning, scrums, software implementation, buy-in, adherence' },
           { point: 'Implemented coding standards' },
           { point: 'Worked with product owners, gathered software requirements, created documentation' },
-          { point: 'Standardized JavaScript and front end concerns.' },
+          { point: 'Standardized javascript and front end concerns.' },
           { point: 'Implemented precompiled, route-aware asset bundling' },
-          { point: 'Used Ruby on Rails, Active Record, Cucumber, Rspec, MySql, JavaScript' }
+          { point: 'Used C#, MVC, MSSql, Entity Framework, SOAP, WCF, JavaScript, HTML, CSS, AJAX' }
         ]
       },
       
@@ -112,7 +111,7 @@ angular
         company: 'Autosoft', 
         title: 'Developer',
         bullets: [
-          { point: 'Responsible for full stack application development using C#, XML, SOAP, WCF, ASP.NET, JQuery, AJAX, and SQL Server'},
+          { point: 'Responsible for full stack application development'},
           { point: 'Led all ESB layer and integrations development efforts' },
           { point: 'Used Fluent NHibernate as a data access framework/ORM' },
           { point: 'Used knowledge of RESTful services, C#, MQ and WMB to implement workflows and WCF ' +
@@ -122,7 +121,8 @@ angular
                    'vehice service history, warranty information, etc'
           },
           { point: 'Maintained current integrations' },
-          { point: 'Implemented new integrations' }     
+          { point: 'Implemented new integrations' }, 
+          { point: 'Used C#, XML, ASP.NET, JQuery, MVC, MSSql, Entity Framework, SOAP, WCF, JavaScript, HTML, CSS, AJAX, WMB' }
         ]
       },
       {
@@ -130,19 +130,17 @@ angular
         company: 'ServiceLink (FNF)', 
         title: 'Infrastructure Architect',
         bullets: [
-          { point: 'Developed software on a task-force team intended to be flexible and provide needed solutions to make development efforts more efficient' },
-          { point: 'Developed a service oriented application to deploy/move code through development, staging, and UAT environments using WCF with both web and winform clients' },
-          { point: 'Designed/Implemented C# applications and services to move log data through subnets/combine log data/parse and analyze log data' },
-          { point: 'Was integral in implementing AWSTATS/JAWSTATS analytics and GEOIP analysis' },
-          { point: 'Was integral in implementing for Nagios monitoring and reported on upwards of 300 servers and twice as many services/applications' },
-          { point: 'Developed/implemented a web portal application which tied together all of the responsibilities above, creating a single touch point for all preproduction data and activities' }, 
+          { point: 'Developed software on a task-force team that created flexible solutions to streamline efficiency' },
+          { point: 'Developed a SOA that promoted/demoted code between development, staging, and UAT' },
+          { point: 'Designed/Implemented C# applications and services to move, parse, analyze and combine meaningful log data across subnets' },
+          { point: 'Helped implement AWSTATS/JAWSTATS analytics and GEOIP analysis' },
+          { point: 'Helped implement Nagios monitoring of 300+ servers and 600+ applications' },
+          { point: 'Developed/implemented an integral web app encorporating all aforementioned responsibilities of this position' }, 
           { point: 'Provided support' },
-          { point: 'Worked closely with various departments to create and implement solutions that ensured functionality, improved efficiency and increased productivity overall' },
-          { point: 'Personally spec\'d, ordered and set up satellite offices in Buffalo, New York and Rancho Cucamonga, California' },
+          { point: 'Personally coordinated all tech acquisition necessary to and set up satellite offices in Buffalo, New York and Rancho Cucamonga, California' },
           { point: 'Handled nearly all technical aspects associated with launching technical operations in these locations' }
         ]
       }
     ]
   })
 ;
-
